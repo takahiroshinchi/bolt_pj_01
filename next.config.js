e69aa567a7b6996a1b52594046a0fef3494d3edf
Takahiro Shinchi <takahiro.shinchi@supership.jp>
2025-04-24 16:42:45 +0900

    Clerk認証修正

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    domains: ['images.unsplash.com'],
+  },
 };
 
 module.exports = nextConfig;
